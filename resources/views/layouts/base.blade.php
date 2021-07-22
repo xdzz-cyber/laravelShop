be4d940c48cca867a272ac885a7669a9b9f0ca1d
Bhbrk <bogdankrut12@gmail.com>
2021-07-22 19:36:02 +0300

    do detail page with its functionality(related items and popular ones are also added), do shopping cart functionality with proper count of items

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -12,6 +12,7 @@
 	<link rel="stylesheet" type="text/css" href="{{asset('assets/css/font-awesome.min.css')}}">
 	<link rel="stylesheet" type="text/css" href="{{asset('assets/css/bootstrap.min.css')}}">
 	<link rel="stylesheet" type="text/css" href="{{asset('assets/css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/flexslider.css')}}">
 	<link rel="stylesheet" type="text/css" href="{{asset('assets/css/chosen.min.css')}}">
 	<link rel="stylesheet" type="text/css" href="{{asset('assets/css/style.css')}}">
 	<link rel="stylesheet" type="text/css" href="{{asset('assets/css/color-01.css')}}">
@@ -165,7 +166,7 @@
 								<a href="#" class="link-direction">
 									<i class="fa fa-shopping-basket" aria-hidden="true"></i>
 									<div class="left-info">
-										<span class="index">4 items</span>
+										<span class="index">{{Cart::count() ?? 0}} items</span>
 										<span class="title">CART</span>
 									</div>
 								</a>
