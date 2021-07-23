f3f4af108b6579c7c00a95cde4013584b7d35187
Bhbrk <bogdankrut12@gmail.com>
2021-07-23 21:07:59 +0300

    add updating cart quantity via UI(using icons), also do deleting single cart item and clearing all items from cart

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -3,9 +3,26 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Cart;
 
 class CartComponent extends Component
 {
+    public function changeQuantity($rowId, $increase){
+        $product = Cart::get($rowId);
+        $qty = $product->qty;
+        $increase === true ? $qty++ : $qty--;
+        Cart::update($rowId, $qty);
+    }
+
+    public function deleteCartItem($rowId){
+        Cart::remove($rowId);
+        session()->flash("success_message", "Cart item has been removed");
+    }
+
+    public function clearCart(){
+        Cart::destroy();
+    }
+
     public function render()
     {
         return view('livewire.cart-component')->layout("layouts.base");
--- resources/views/livewire/cart-component.blade.php
+++ resources/views/livewire/cart-component.blade.php
@@ -31,13 +31,13 @@
                                 <div class="quantity">
                                     <div class="quantity-input">
                                         <input type="text" name="product-quatity" value="{{$item->qty}}" data-max="120" pattern="[0-9]*" >
-                                        <a class="btn btn-increase" href="#"></a>
-                                        <a class="btn btn-reduce" href="#"></a>
+                                        <a class="btn btn-increase" href="#" wire:click.prevent="changeQuantity('{{$item->rowId}}', true)"></a>
+                                        <a class="btn btn-reduce" href="#" wire:click.prevent="changeQuantity('{{$item->rowId}}', false)"></a>
                                     </div>
                                 </div>
                                 <div class="price-field sub-total"><p class="price">${{$item->subtotal}}</p></div>
                                 <div class="delete">
-                                    <a href="#" class="btn btn-delete" title="">
+                                    <a href="#" class="btn btn-delete" wire:click.prevent="deleteCartItem('{{$item->rowId}}')" title="">
                                         <span>Delete from your cart</span>
                                         <i class="fa fa-times-circle" aria-hidden="true"></i>
                                     </a>
@@ -66,7 +66,7 @@
 						<a class="link-to-shop" href="shop.html">Continue Shopping<i class="fa fa-arrow-circle-right" aria-hidden="true"></i></a>
 					</div>
 					<div class="update-clear">
-						<a class="btn btn-clear" href="#">Clear Shopping Cart</a>
+						<a class="btn btn-clear" href="#" wire:click.prevent="clearCart()">Clear Shopping Cart</a>
 						<a class="btn btn-update" href="#">Update Shopping Cart</a>
 					</div>
 				</div>
